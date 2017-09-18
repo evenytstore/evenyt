7c48959b654a702756cff0264da0544036b8f505
jdbustamante <goldfenrir@hotmail.com>
2017-09-17 22:58:56 -0500

    beauty

--- Evenytstore/app/src/main/java/com/app/evenytstore/Activity/MainActivity.java
+++ Evenytstore/app/src/main/java/com/app/evenytstore/Activity/MainActivity.java
@@ -125,17 +125,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.categoryList) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.myAccount) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.myOrders) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.myLists) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.email) {
+
+        } else if (id == R.id.telephone) {
 
         }
 
